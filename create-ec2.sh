335e5464fc36c1140690e26b4c6eff8795910740
virinchikvl <virinchikvl9@gmail.com>
2024-03-12 11:36:17 +0530

    ansible

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,13 +6,13 @@ SECURITY_GROUP="sg-0aa143e518dfb2e87"
 IMAGE_ID="0f3c7d07486cad139"
 for i in "${NAMES[@]}"
 do 
-		if [ [ $i == "mongodb" || $i == "mysql" ] ]
+		if [[ $i == "mongodb" || $i == "mysql" ]]
 		then 
 				INSTANCE_TYPE="t3.medium"
 				else
 				INSTANCE_TYPE="t2.micro"
         fi       
 		echo " creating  $i instance" 
-		    IP_ADDRESS=$(aws ec2 run-instances --image-id ami  $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids sg-0aa143e518dfb2e87 |  jq -r '.Instances[0].PrivateIpAddress' )
+		    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP |  jq -r '.Instances[0].PrivateIpAddress' )
 		echo " created  $i instance : $IP_ADDRESS" 
 done
